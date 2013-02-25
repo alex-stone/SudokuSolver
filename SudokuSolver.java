d92944a522dd43c71b7d3b6664cdd5cc38ea9c38
Alex Stone <alexstone@berkeley.edu>
2013-02-24 17:37:06 -0800

    Initial SudokuSolver - Empty Constructor and Solve function

---
+++ SudokuSolver.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class SudokuSolver {
+    
+    /**
+     * 
+     */
+    public SudokuSolver() {
+
+    }
+
+    /**
+     * Attempts to solve the Sudoku Puzzle and returns True if successful, and False if not.
+     *
+     * @return the boolean value representing if the puzzle was successfully solved
+     */
+    public boolean solve() {
+
+
+    }
+}
