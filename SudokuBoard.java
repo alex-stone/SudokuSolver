7806bebefd837ac629b67efa8c8fd30c9d0d34cd
Alex Stone <alexstone@berkeley.edu>
2013-02-24 17:49:47 -0800

    Added SudokuBoard w/ general methods needed

---
+++ SudokuBoard.java
@@ -0,0 +1,50 @@
+
+
+public class SudokuBoard() {
+
+    private int [][] matrix;
+    
+    /**
+     * Initialize's a blank board with all 0's.
+     */
+    public SudokuBoard() {
+
+    }
+
+    /**
+     * Initializes the SudokuBoard to the values from the 9x9 array
+     */
+    public SudokuBoard(int[][] array) {
+
+	// Test if the array is 9x9
+
+
+    }
+
+
+    /**
+     * Checks if the current board is valid
+     */
+    public boolean isBoardValid()  {
+
+    }
+
+    /**
+     * Checks if the board is valid given this new entry value
+     */
+    public boolean isBoardValid(int row, int col, int value) {
+
+
+    }
+   
+    /**
+     * Returns whether or not the current board is solved, meaning it is both valid
+     * and completed.
+     */
+    public boolean isBoardSolved() {
+
+
+    }
+
+     
+}
